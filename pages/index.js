70409b7b5801a61b0e28f34c622a3df1c46c9b07
Alex Sandro <elalex1433@gmail.com>
2026-05-12 13:09:02 +0000

    adicioana scripts `lint:check`  e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,8 @@ function Home(){
     return <><h1> Victória, você vai tirar notas baixa em todas as Matérias!</h1>
     </>
 }
+
+function teste(){
+      console.log('teste');
+}
 export default Home;
